6392a3c636b4e80ef54cee190188c88f49f165aa
Arga Adinata <argaadinata10@gmail.com>
2024-10-09 10:07:57 +0700

    UTS

--- app/src/main/java/com/example/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan6/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.DatePicker.OnDateChangedListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pertemuan6.databinding.ActivityMainBinding
 import java.util.Calendar
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,20 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        with(binding){
+        val kehadiranList = listOf("Hadir tepat waktu", "Terlambat", "Izin")
+
+        with(binding) {
 //            Get Array
             val monthList = resources.getStringArray(R.array.month)
 
 //            Initiate
-            var selectedTime ="${timePicker.hour}:${timePicker.minute}"
-            val _tempCalendar : Calendar = Calendar.getInstance()
+            var selectedTime = "${timePicker.hour}:${timePicker.minute}"
+            val _tempCalendar: Calendar = Calendar.getInstance()
             _tempCalendar.timeInMillis = System.currentTimeMillis()
-            var selectedDate = "${_tempCalendar.get(Calendar.DAY_OF_MONTH)} ${monthList[_tempCalendar.get(Calendar.MONTH)]} ${_tempCalendar.get(Calendar.YEAR)}"
+            var selectedDate =
+                "${_tempCalendar.get(Calendar.DAY_OF_MONTH)} ${monthList[_tempCalendar.get(Calendar.MONTH)]} ${
+                    _tempCalendar.get(Calendar.YEAR)}"
 
+            datepicker.init(
+                datepicker.year,
+                datepicker.month,
+                datepicker.dayOfMonth
+            ) { _, year, month, day ->
+                selectedDate = "$day ${monthList[month]} $year"
+            }
+
+            timePicker.setOnTimeChangedListener { view, hour, minute ->
+                selectedTime = String.format("%02d:%02d", hour, minute)
+            }
 
 //            Kehadiran Dropdown=======================================
             val adapterKehadiran = ArrayAdapter<String>(
-                this,
+                this@MainActivity,
                 android.R.layout.simple_spinner_item,
                 kehadiranList
             )
@@ -40,16 +53,30 @@ class MainActivity : AppCompatActivity() {
 //            Selected Kehadiran
             kehadiranSpinner.onItemSelectedListener =
                 object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        if (position == 1 || position == 2) {
+                            keteranganEdittext.visibility = View.VISIBLE
+                        } else if (position == 0) {
+                            keteranganEdittext.visibility = View.GONE
+                        }
                     }
 
                     override fun onNothingSelected(parent: AdapterView<*>?) {
-
                     }
                 }
 
-
+            submitButton.setOnClickListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Presensi berhasil $selectedDate jam $selectedTime",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
 
         }
     }
